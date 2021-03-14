f092ef797b5066189dd804c46e55935209623d6c
Grey Elerson <grey.l.elerson@gmail.com>
2021-03-13 17:44:21 -0700

    add bootstrap card formatting to decks display page

--- src/Layout/DecksDisplay/index.js
+++ src/Layout/DecksDisplay/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React from "react";
 
 export default function DecksDisplay({ decks }) {
-    const decksForDisplay = decks.map(deck => { return <p>{deck.name}</p>})
-    return (<div>{decksForDisplay}</div>);
+  const decksForDisplay = decks.map((deck) => {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{deck.name}</h5>
+          <p className="card-text">{deck.description}</p>
+
+          <div className="d-flex">
+          <button class="btn btn-primary" type="button">
+            Button
+          </button>
+          <button class="btn btn-primary" type="button">
+            Button
+          </button>
+          <button class="btn btn-primary ml-auto" type="button">
+            Button
+          </button>
+          </div>
+          
+        </div>
+      </div>
+    );
+  });
+
+  return <React.Fragment>{decksForDisplay}</React.Fragment>;
 }
